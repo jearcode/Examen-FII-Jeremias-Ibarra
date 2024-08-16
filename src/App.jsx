242b3b641c3af59bcad60f261b926d774ccc7c93
jearcode <jereibarra10@outlook.es>
2024-08-15 20:47:08 -0500

    Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,74 @@
+import { useState } from "react";
+import Card from "./Card";
+import Styles from "./styles/Form.module.css"
+
 function App() {
   //Aqui deberias agregar los estados y los handlers para los inputs
 
+  const [userName, setUserName] = useState('')
+  const [userDNI, setUserDNI] = useState('')
+
+  const [greenFlag, setGreenFlag] = useState(false)
+  const [redFlag, setRedFlag] = useState(false)
+
+  const userData = {
+    userName: userName,
+    userDNI: userDNI
+  }
+
+  console.log(userData);
+  
+
+  const onChangeUserName = (e) =>{
+    setUserName(e.target.value)
+  }
+
+  const onChangeDNI = (e) =>{
+    setUserDNI(e.target.value)
+  }
+
+  const hasLeadingSpaces = (input) =>{
+    const regex = /^\s+/
+    return regex.test(input)
+  }
+
+  const validateUserName = () =>{
+    if(userName.length > 3 && !hasLeadingSpaces(userName)){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  const validateUserDNI = () =>{
+    if(userDNI.length >= 6){
+      return true
+    }
+    return false
+  }
+
+  const handlerSubmit = (e) =>{
+    e.preventDefault()
+    if(validateUserName() && validateUserDNI()){
+      setGreenFlag(true)
+      if(redFlag === true){
+        setRedFlag(false)
+      }
+    }else{
+      setRedFlag(true)
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1 className={Styles.text}>¡Hola! Ingresa tus datos</h1>
+      <form className={Styles.container} onSubmit={handlerSubmit}>
+        <input className={Styles.input} onChange={onChangeUserName} type="text" placeholder="Ingresa tu nombre"/>
+        <input className={Styles.input} onChange={onChangeDNI} type="number" placeholder="Ingresa tu DNI"/>
+        <button className={Styles.button} type="submit">ENVIAR</button>
+      </form>
+      {greenFlag && <Card userData={userData}/>}
+      {redFlag && <p className={Styles.text} style={{color: "red"}}>Por favor chequea que la información sea correcta</p>}
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,12 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
+import Styles from './styles/Card.module.css'
 
-function Card() {
+function Card({userData}) {
   return (
-    <div>
-      <h2>Esto es un componente</h2>
+    <div className={Styles.card}>
+      <h2 className={Styles.text}>Hola {userData.userName}</h2>
+      <p className={Styles.text}>Número nacional de identidad: {userData.userDNI}</p>
     </div>
   );
 }
